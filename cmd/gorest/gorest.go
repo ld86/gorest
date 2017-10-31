c7c8b3413e26902177ccad4ad2b0b6d3afdef08a
Bogdan Melnik <teh.ld86@gmail.com>
2017-10-31 08:37:54 +0300

    Add view of key

--- cmd/gorest/gorest.go
+++ cmd/gorest/gorest.go
@@ -4,6 +4,7 @@ import (
     "fmt"
 	"io"
 	"sync"
+	"regexp"
     "net/http"
 	"encoding/json"
 )
@@ -17,6 +18,11 @@ func (storage *KeysStorage) AddKey(key string, value interface{}) {
 	storage.Mutex.Lock()
 	storage.Keys[key] = value
 	storage.Mutex.Unlock()
+}
+
+func (storage *KeysStorage) GetKey(key string) (value interface{}, found bool) {
+	value, found = storage.Keys[key]
+	return
 }
 
 type KeysResponse struct {
@@ -76,8 +82,36 @@ func addOrViewAll(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s", responseJson)
 }
 
+var keysRegexp, _ = regexp.Compile("/keys/(.*)")
 func viewOrDelete(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%s", r)
+	response := KeysResponse{}
+	switch r.Method {
+		case "GET":
+			matches := keysRegexp.FindStringSubmatch(r.URL.String())
+			key := matches[1]
+			if key == "" {
+				response.OutputError("Key cannot be empty")
+				break
+			}
+			value, found := keysStorage.GetKey(key)
+			if !found {
+				response.OutputError("Cannot find key")
+				break
+			}
+
+			response.Status = "ok"
+			data, err := json.Marshal(value)
+			if err != nil {
+				response.OutputError("Internal storage error")
+				break
+			}
+
+			response.Data = string(data)
+		default:
+			response.OutputError("Wrong method")
+	}
+	responseJson, _ := json.Marshal(response)
+	fmt.Fprintf(w, "%s", responseJson)
 }
 
 func main() {
